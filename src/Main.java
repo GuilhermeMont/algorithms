2b31610dc5eeb8346fe323183814353f5f76fc79
GuilhermeMont <guilherme.monteiro@usermobile.xyz>
2019-12-03 16:44:26 -0300

    generate graph instances

--- src/Main.java
+++ src/Main.java
@@ -43,12 +43,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File[] graphInstances = { new File("graph.txt"),
-                new File("graph3.txt"),
-                new File("graph2.txt") };
+        File[] graphInstances = { new File("graph.txt")};
 
         generateProblems(graphInstances);
 
+
+//        graphGenerator gg = new graphGenerator();
+//        gg.generate(100,"graph100-1.txt");
+//        gg.generate(100,"graph100-2.txt");
+//        gg.generate(400,"graph400-1.txt");
+//        gg.generate(400,"graph400-2.txt");
+//        gg.generate(200,"graph200.txt");
+//        gg.generate(200,"graph200-1.txt");
+//        gg.generate(200,"graph200-2.txt");
+//        gg.generate(300,"graph300.txt");
+//        gg.generate(300,"graph300-1.txt");
+//        gg.generate(300,"graph300-2.txt");
+//        gg.generate(500,"graph500.txt");
+//        gg.generate(500,"graph500-1.txt");
+//        gg.generate(500,"graph500-2.txt");
 
 
     }
